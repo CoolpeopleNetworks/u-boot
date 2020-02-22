59c06ef5c34187ae6810c593b5cc992a9c41918b
John W. Terrell <john@coolpeoplenetworks.com>
2020-02-21 16:35:57 -0800

    Force BeagleBone Black

--- board/ti/am335x/board.h
+++ board/ti/am335x/board.h
@@ -30,7 +30,8 @@ static inline int board_is_bone(void)
 
 static inline int board_is_bone_lt(void)
 {
-	return board_ti_is("A335BNLT");
+//	return board_ti_is("A335BNLT");
+	return 1;
 }
 
 static inline int board_is_pb(void)
